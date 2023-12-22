0ff883996057c5030c9f0307cd9616a757226078
shashaaankkkkk <shashankshekhar8534@gmail.com>
2023-12-22 17:20:59 +0530

    added basic of c

--- array.c
+++ array.c
@@ -2,10 +2,11 @@
 int main(){
   //datatype name_of_array [length of array]={ all the data here using ,};
   int arr[10]={1,2,3,4,5};
+  int *ptr = arr;
   //  Array Traversal using for Loop
-  for (int i = 0; i < 5; i++) {
+  for (int i=0; i < 5; i++) {
     // array[index] index calling of an array
-    printf("%d\n",arr[i]);
+    printf("%d\n",*ptr+i);
   }
-  
+  return 0;
 }
--- calculator.c
+++ calculator.c
@@ -4,7 +4,7 @@ int main ()
     int n,m,b;
     printf("Choose your integers\n");
     scanf("%d%d",&m,&b);
-    printf("Which function would you want to use?\n1. Multiplication\n2. Division\n3 Addition\n4. Substraction");
+    printf("Which function would you want to use?\n1. Multiplication\n2. Division\n3 Addition\n4. Substraction\n");
     scanf("%d",&n);
     if (n==1)
     {
@@ -17,8 +17,12 @@ int main ()
     {
         printf("m+b=%d", m+b);
     }
-    else{
+    else if (n==4){
         printf("m-b=%d", m-b);
+        printf("\n");
+    }
+    else{
+      printf("Wrong Choice ! Enter correct option\n");
     }
 return 0;
 }

47f9309d5d3f22e0c35f1acd9a1a0233eb369008
shashank <shashankshekhar8534@gmail.com>
2024-03-19 00:00:00 +0530

    Add c_learning_journal/05_factorial_function.c

---
+++ c_learning_journal/05_factorial_function.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int factorial(int n) {
+    if (n == 0) return 1;
+    return n * factorial(n - 1);
+}
+
+int main() {
+    printf("Factorial of 5: %d\n", factorial(5));
+    return 0;
+}

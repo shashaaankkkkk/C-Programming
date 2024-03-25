f71d385fbde5e95feb8dee57f52092fcfcdb2b49
shashank <shashankshekhar8534@gmail.com>
2024-03-26 00:00:00 +0530

    Add c_learning_journal/03_simple_calculator.c

---
+++ c_learning_journal/03_simple_calculator.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    int a = 10, b = 5;
+    printf("Sum: %d\n", a + b);
+    printf("Difference: %d\n", a - b);
+    printf("Product: %d\n", a * b);
+    printf("Quotient: %d\n", a / b);
+    return 0;
+}

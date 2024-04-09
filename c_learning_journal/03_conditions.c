9420e0b429e84c54c0a6ba40f699a0ded2203487
shashank <shashankshekhar8534@gmail.com>
2024-04-10 00:00:00 +0530

    Add c_learning_journal/03_conditions.c

---
+++ c_learning_journal/03_conditions.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    int number = 15;
+    if (number > 10) {
+        printf("%d is greater than 10\n", number);
+    } else {
+        printf("%d is not greater than 10\n", number);
+    }
+    return 0;
+}

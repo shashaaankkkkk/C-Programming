26f22040870ff2c0a73d0d2817f072926437da7f
shashank <shashankshekhar8534@gmail.com>
2024-04-12 00:00:00 +0530

    Add c_learning_journal/02_data_types_demo.c

---
+++ c_learning_journal/02_data_types_demo.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+    int a = 5;
+    float pi = 3.14;
+    char letter = 'C';
+    printf("Integer: %d, Float: %.2f, Char: %c\n", a, pi, letter);
+    return 0;
+}

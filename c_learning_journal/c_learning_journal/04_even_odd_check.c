c804021d64e6426aab6a748158da4cba5dcf89e7
shashank <shashankshekhar8534@gmail.com>
2024-08-17 00:00:00 +0530

    Add c_learning_journal/04_even_odd_check.c

---
+++ c_learning_journal/c_learning_journal/04_even_odd_check.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    int num = 10;
+    if (num % 2 == 0) {
+        printf("%d is even\n", num);
+    } else {
+        printf("%d is odd\n", num);
+    }
+    return 0;
+}
